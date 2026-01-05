b2ab31cff783249940b360f6041f8a12f7656191
Asim-AKM <164513926+Asim-AKM@users.noreply.github.com>
2026-01-05 19:23:01 +0500

    API Integration

---
+++ Netrex.Frontend.Blazor/Models/VMUserRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Netrex.Frontend.Blazor.Models
+{
+    public class VMUserRegistration
+    {
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+        [JsonPropertyName("contact")]
+        public string Contact  { get; set; }
+        [JsonPropertyName("username")]
+        public string UserName { get; set; }
+
+    }
+}
---
+++ Netrex.Frontend.Blazor/Services/AuthService.cs
@@ -0,0 +1,20 @@
+﻿using Netrex.Frontend.Blazor.Models;
+
+namespace Netrex.Frontend.Blazor.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthService(IHttpClientFactory factory)
+        {
+            _httpClient = factory.CreateClient("ApiClient");
+        }
+
+        public async Task<bool> UserRegisterAsync(VMUserRegistration obj)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/Authentication/create", obj);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}

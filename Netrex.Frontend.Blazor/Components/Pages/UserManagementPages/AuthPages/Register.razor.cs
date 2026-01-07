fdca4a3a55a2d1786f3eac3a4b733694acfb6e8f
Asim-AKM <164513926+Asim-AKM@users.noreply.github.com>
2026-01-07 19:04:08 +0500

    ui

---
+++ Netrex.Frontend.Blazor/Components/Pages/UserManagementPages/AuthPages/Register.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Netrex.Frontend.Application.Services.UserManagement.Interfaces;
+using Netrex.Frontend.Application.ViewModels.UserManagement.Authentication;
+
+namespace Netrex.Frontend.Blazor.Components.Pages.UserManagementPages.AuthPages
+{
+    public partial class Register
+    {
+
+        [Inject]
+        public IAuthManager? _authManager { get; set; }
+        RegisterViewModel _model = new RegisterViewModel();
+        public async Task HandleRegister()
+        {
+            await _authManager!.RegisterAsync(_model);
+        }
+
+    }
+}

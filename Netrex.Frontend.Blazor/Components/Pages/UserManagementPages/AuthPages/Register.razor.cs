006b35555da056726456e27f746328f0a435c0a0
Shoaib96978 <muhammadshoaob26@gmail.com>
2026-01-08 09:22:30 +0500

    Feture : Add some files in registeration flow

---
+++ Netrex.Frontend.Blazor/Components/Pages/UserManagementPages/AuthPages/Register.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Netrex.Frontend.Application.Services.UserManagement.Interfaces;
+using Netrex.Frontend.Application.ViewModels.UserManagement.Authentication;
+
+namespace Netrex.Frontend.Blazor.Components.Pages.UserManagementPages.AuthPages
+{
+    public partial class Register
+    {
+        [Inject]
+        private IAuthManager _authManager { get; set; }
+        RegisterViewModel _model = new RegisterViewModel();
+        public async Task HandleSignUp()
+        {
+            await _authManager.RegisterAsync(_model);
+        }
+    }
+}

6c99c63ac5d3805d587a60671a14c5338e011602
MuhammadAshar <muhammadashar.7100@gmail.com>
2026-01-22 16:13:48 +0500

    Implement InterFace and implementation

---
+++ Netrex.Frontend.Application/Services/SellerAndShop/Interfaces/ISellerAndshop.cs
@@ -0,0 +1,20 @@
+﻿using Netrex.Frontend.Application.ViewModels.SellerModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netrex.Frontend.Application.Services.SellerAndShop.Interfaces
+{
+    public interface ISellerAndshop
+    {
+       
+        Task<string> CreateSellerAsync(VmSeller vmSeller);
+        Task<string> UpdateSellerAsync(VmSeller vmSeller);
+        Task<string> DeleteSellerAsync(Guid Id);
+        Task<List<VmSeller>> GetSellerAsync(); 
+        Task<VmSeller>   GetBYIDSellerAsync(Guid Id);
+
+    }
+}

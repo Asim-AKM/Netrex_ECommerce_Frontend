ff2e672cc7376a77779eef37998a5d825000dc02
DaaDu123 <saadmuhammad19115@gmail.com>
2026-02-05 09:14:48 +0500

    hotfix: VerifiyEmail And VerifyEmail ResetPassword and CustomerManager

---
+++ Netrex.Frontend.Application/Services/Customer/Interfaces/ICustomerManager.cs
@@ -0,0 +1,9 @@
+﻿using Netrex.Frontend.Application.DTO_s;
+
+namespace Netrex.Frontend.Application.Services.Customer.Interfaces
+{
+    public interface ICustomerManager
+    {
+        Task<bool> UpdateCustomerAsync(UpdateCustomerDto customer);
+    }
+}

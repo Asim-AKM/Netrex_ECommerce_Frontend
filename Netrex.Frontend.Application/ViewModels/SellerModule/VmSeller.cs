0467b9be924600feda8f25754a2fee87897f1a02
MuhammadAshar <muhammadashar.7100@gmail.com>
2026-02-10 19:01:30 +0500

    fect: AddValidationSellerRegisterpage

--- Netrex.Frontend.Application/ViewModels/SellerModule/VmSeller.cs
+++ Netrex.Frontend.Application/ViewModels/SellerModule/VmSeller.cs
@@ -1,22 +1,30 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
-namespace Netrex.Frontend.Application.ViewModels.SellerModule
+public class VmSeller
 {
-    public class VmSeller
-    {
-        [JsonPropertyName("SellerId")]
-        public Guid SellerId { get; set; }
-        [JsonPropertyName("UserId")]
-        public Guid UserId { get; set; }
-        [JsonPropertyName("ShopId")]
-        public Guid ShopId { get; set; }
-        [JsonPropertyName("StoreName")]
-        public string StoreName { get; set; } = string.Empty;
-        [JsonPropertyName("StoreDescription")]
-        public string StoreDescription { get; set; } = string.Empty;
-        [JsonPropertyName("Address")]
-        public string StoreAddress { get; set; }   = string.Empty;
-    }
+    [JsonPropertyName("SellerId")]
+    public Guid SellerId { get; set; }
 
+    [Required(ErrorMessage = "User is required")]
+    [JsonPropertyName("UserId")]
+    public Guid UserId { get; set; }
+
+    [Required(ErrorMessage = "Shop category is required")]
+    [JsonPropertyName("ShopId")]
+    public Guid ShopId { get; set; }
+
+    [Required(ErrorMessage = "Store name is required")]
+    [MinLength(3, ErrorMessage = "Store name must be at least 3 characters")]
+    [JsonPropertyName("StoreName")]
+    public string StoreName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Store description is required")]
+    [MinLength(10, ErrorMessage = "Description must be at least 10 characters")]
+    [JsonPropertyName("StoreDescription")]
+    public string StoreDescription { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Store address is required")]
+    [JsonPropertyName("Address")]
+    public string StoreAddress { get; set; } = string.Empty;
 }
-

ce008c688d355c9581ad6f3b3ac395be948da307
DaaDu123 <saadmuhammad19115@gmail.com>
2026-02-13 17:51:01 +0500

    Add customer Profile Add Upload Profile From and PC

---
+++ Netrex.Frontend.Application/Services/Customer/Implementation/CustomerManager.cs
@@ -0,0 +1,38 @@
+﻿using Netrex.Frontend.Application.DTO_s;
+using Netrex.Frontend.Application.Services.Customer.Interfaces;
+using System.Net.Http.Json;
+using System.Net.Http.Headers;
+
+namespace Netrex.Frontend.Application.Services.Customer.Implementation
+{
+    // Primary Constructor use ho raha hai
+    public class CustomerManager(HttpClient http) : ICustomerManager
+    {
+        public async Task UpdateCustomerAsync(UpdateCustomerDto customer)
+        {
+            var response = await http.PutAsJsonAsync("api/Customer/updateCustomer", customer);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Failed to update customer");
+            }
+        }
+
+        public async Task UpdateProfileImageAsync(Guid userId, byte[] imageData)
+        {
+            using var content = new MultipartFormDataContent();
+            var fileContent = new StreamContent(new MemoryStream(imageData));
+            fileContent.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+
+            content.Add(fileContent, "File", "profile.jpg");
+            content.Add(new StringContent(userId.ToString()), "UserId");
+
+            var response = await http.PostAsync("api/customer/updateProfileImage", content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Image upload failed");
+            }
+        }
+    }
+}
---
+++ Netrex.Frontend.Application/Services/Customer/Interfaces/ICustomerManager.cs
@@ -0,0 +1,10 @@
+﻿using Netrex.Frontend.Application.DTO_s;
+
+namespace Netrex.Frontend.Application.Services.Customer.Interfaces
+{
+    public interface ICustomerManager
+    {
+        Task UpdateCustomerAsync(UpdateCustomerDto customer);
+        Task UpdateProfileImageAsync(Guid userId, byte[] imageData);
+    }
+}

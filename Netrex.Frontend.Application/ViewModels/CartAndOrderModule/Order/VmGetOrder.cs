7d3235e2cbb5723e1b8750c34ca602a00ed5c03f
MuhammadShehzad709 <muhammadshehzaddeveloper@gmail.com>
2026-03-12 18:08:45 +0500

    Here Customer Cart And Order Pages Are Dynamicaly Arranged

---
+++ Netrex.Frontend.Application/ViewModels/CartAndOrderModule/Order/VmGetOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace Netrex.Frontend.Application.ViewModels.CartAndOrderModule.Order
+{
+    public class VmGetOrder
+    {
+        public Guid OrderId { get; set; }
+        public bool OrderStatus { get; set; }
+        public double TotalAmount {  get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}

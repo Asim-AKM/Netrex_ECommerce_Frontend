f379e5d8977b6f7dd8f003aacec98e26c1bc18a9
saqibkhan4597 <saqibkhan4597@gmail.com>
2026-03-17 12:14:03 +0500

    Feature: Add chatbot to the home page(main layout).

---
+++ Netrex.Frontend.Application/Services/ChatBot/Interfaces/IChatBotManager.cs
@@ -0,0 +1,10 @@
+﻿using Netrex.Frontend.Application.Commons.AppResponses;
+using Netrex.Frontend.Application.ViewModels.ChatBotVMS;
+
+namespace Netrex.Frontend.Application.Services.ChatBot.Interfaces
+{
+    public interface IChatBotManager
+    {
+        Task<ApiResponse<ChatResponseVM>> SendMessageAsync(string message);
+    }
+}
